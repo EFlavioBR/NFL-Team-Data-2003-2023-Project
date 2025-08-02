e099829ed371e87e9d8fa31f6ce4b0085cba7282
Emanuel Flávio <emanuelflavio@alu.ufc.br>
2025-08-02 03:50:23 -0300

    5 seasons mais vencedoras do panthers

--- datasetnfl.py
+++ datasetnfl.py
@@ -5,3 +5,13 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 panthers_df = df[df['team'] == 'Carolina Panthers'].copy()
 print(panthers_df)
+
+#ordenando o df pela a coluna de vitórias em ordem decrescente
+top_seasons = panthers_df.sort_values(by='wins', ascending=False)
+
+top_5_seasons = top_seasons.head(5)
+
+print("\nPanthers")
+print(top_5_seasons[['year', 'wins']])
+print(top_5_seasons.mean())
+
